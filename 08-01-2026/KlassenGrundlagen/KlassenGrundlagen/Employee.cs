2063171b25bb77f132328ebba882f9deac4fd87a
dilara <homeoffice.dilara@gmail.com>
2026-01-08 21:32:05 +0100

    personal angelegt

---
+++ 08-01-2026/KlassenGrundlagen/KlassenGrundlagen/Employee.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace KlassenGrundlagen
+{
+    public class Employee
+    {
+        public string Name;
+        public string Surname;
+        public Guid id;
+        public decimal Sallery;
+        public DateTime Birthday;
+
+        //user spezific construcker, dürfen meherere haben  
+        public Employee(string name, string surname, DateTime birthday)
+        {
+            Name = name;
+            Surname = surname;
+            Birthday = birthday;
+
+            id = Guid.NewGuid();
+            Sallery = 1500.0m;
+        }
+
+        //bonus gehalt
+        public void GiveBonus(double bonusInPercent)
+        {
+            if (bonusInPercent > 0 && bonusInPercent <= 1)
+            {
+                Sallery += Sallery * (decimal)bonusInPercent;
+
+                int age = DateTime.Now.Year - Birthday.Year;
+
+                decimal maxSallery = age * 100;
+                if (Sallery > maxSallery)
+                {
+                    Sallery = maxSallery;
+                }
+
+            }
+        }
+
+        //methoden/Logik
+        public void Display()
+        {
+            Console.WriteLine($"{Name} {Surname}");
+            Console.WriteLine($"ID: {id}");
+
+        }
+    }
+}
---
+++ 08-01-2026/KlassenGrundlagen/KlassenGrundlagen/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace KlassenGrundlagen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanzierung deshalb auch new weil wir etwas neues erzeugen
+            Employee ma1 = new Employee("Max", "Musterman", new DateTime(2006, 02, 15));
+          
+            ma1.Name = "Max";
+            ma1.Surname = "Musterman";
+            ma1.id = Guid.NewGuid();
+            ma1.Sallery = 2000.0m;
+            ma1.Birthday = new DateTime(2006, 02, 15);
+
+            ma1.Display();
+
+            //bonud geben
+            ma1.GiveBonus(0.2);
+
+            Console.WriteLine($"Neues Gehalt: EUR {ma1.Sallery.ToString("#,###.00")})");
+
+
+            //wörter
+            //klasse ist wie manusktipt
+            //onjekt ist wie eine kopie von manuskipt also 
+            //von der klasse wird das Objekt und Instanz
+            //von der klasse Employee eine ma1 instanz hergestellt
+
+          
+        }
+    }
+}
